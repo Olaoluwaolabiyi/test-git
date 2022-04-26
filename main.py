6706b60686dd5d6faccc38da0a9291ce02f9916c
Ola <olaoluwaolabiyi@yahoo.com>
2022-04-26 21:49:10 +0200

    Pandas DataFrame

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import pandas as pd
+import numpy as np
+
+people = {
+    'name': ['Ola', 'Solo', 'Ugo'],
+    'sex': ['Male', 'Male','Male'],
+    'age': [1,2,3]
+}
+
+peopledf = pd.DataFrame(data=people)
+
 print('Hello_world')
-print(dir())
+print(peopledf)
